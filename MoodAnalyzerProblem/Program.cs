625878bd6040802c5c35d8b5439eb00d1bcbbd60
rajpal229 <rajpalrawat229@gmail.com>
2022-12-22 23:49:23 +0530

    Added: Refactor_TC2.1

--- MoodAnalyzerProblem/Program.cs
+++ MoodAnalyzerProblem/Program.cs
@@ -4,17 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Checking for Happy");
-            MoodAnalyzer moodAnalyzer =new MoodAnalyzer("HaPPy");
-            moodAnalyzer.AnalyzeMood();
-
-            Console.WriteLine("\nChecking for Sad");
+            Console.WriteLine("Checking for Sad");
             MoodAnalyzer moodAnalyzer1 = new MoodAnalyzer("SaD");
             moodAnalyzer1.AnalyzeMood();
-
-            Console.WriteLine("\nChecking for Any other input");
-            MoodAnalyzer moodAnalyzer2 = new MoodAnalyzer("sasd");
-            moodAnalyzer2.AnalyzeMood();
         }
     }
 }
